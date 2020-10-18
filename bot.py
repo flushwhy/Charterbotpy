88c7d90ce2f8ab90bdec0df8e5ca1e84f80e4308
Austin <roflush@pm.me>
2020-10-18 14:41:35 -0400

    Finished the Morse code translator
    I also added a new game status

--- bot.py
+++ bot.py
@@ -2,6 +2,7 @@ import discord
 from discord.ext import commands
 
 import sys, traceback
+import psutil
 
 get_token = open('token.txt', 'r').read()
 
@@ -19,6 +20,8 @@ initial_extensions = [ 'cogs.nema',
                        'cogs.meta', ]
 
 
+    #CPU and RAM display for game output
+
 bot = commands.Bot(command_prefix=get_prefixes, description='Charter Bot is the only chartered bot!')
 
 if __name__ == '__main__':
@@ -29,8 +32,12 @@ if __name__ == '__main__':
 async def on_ready():
     print(f'\n\nLogged in as: {bot.user.name} - {bot.user.id}\nVersion: {discord.__version__}\n')
 
+    #CPU and RAM display for game output
+    mem = psutil.virtual_memory()
+    cpu = psutil.cpu_percent(interval=None)
+
     await bot.change_presence(status=discord.Status.online,
-    activity=discord.Game(f'USE \'. , ? , or cb.\' TO TEST COMMANDS!'))
+    activity=discord.Game(f'Mem used: {mem.used}, CPU used: {cpu}'))
     print(f'Successfully logged in and booted...!')
 
 
